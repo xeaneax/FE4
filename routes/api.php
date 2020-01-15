2d6d30fd9c86d838f104b3d373a46d850f23ab66
Joowd <joowdx@pm.me>
2020-01-15 13:26:47 +0800

    re-route exercise-3 store

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::any('exercise-1', 'Exercise1');
 Route::any('exercise-2', 'Exercise2');
 Route::any('exercise-3', 'Exercise3@index');
-Route::any('exercise-3-store', 'Exercise3@store');
+Route::any('exercise-3/store', 'Exercise3@store');
